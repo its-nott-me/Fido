d6a4af8bd11a67762951b64c94096299a154e105
Vivek <2105vivek@gmail.com>
2026-01-18 23:15:18 +0530

    update schema.sql

--- server/schema/migrations/schema.sql
+++ server/schema/migrations/schema.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
   username VARCHAR(50) UNIQUE NOT NULL,
   password_hash TEXT NOT NULL,
+  profile_image_key TEXT,
   created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
